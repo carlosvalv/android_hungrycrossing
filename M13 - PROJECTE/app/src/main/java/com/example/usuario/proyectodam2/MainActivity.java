e18c343937eed8b4a8e4561909845d99a8bb5c40
bernatuport15 <bernatuport15@gmail.com>
2017-05-09 12:14:09 +0200

    Nova classe
    He creat nova classe per la conecio amb el host , es asyntask i part del
    codig del main pasara a aquesta classe

--- M13 - PROJECTE/app/src/main/java/com/example/usuario/proyectodam2/MainActivity.java
+++ M13 - PROJECTE/app/src/main/java/com/example/usuario/proyectodam2/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.usuario.proyectodam2;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -43,6 +48,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 HttpURLConnection urlConnection = null;
                 urlConnection = (HttpURLConnection)url.openConnection();
                 status = urlConnection.getResponseCode();
+
+
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setReadTimeout(10000 /* milliseconds */);
+                urlConnection.setConnectTimeout(15000 /* milliseconds */);
+
+                urlConnection.setDoOutput(true);
+
+                urlConnection.connect();
+
+                BufferedReader br=new BufferedReader(new InputStreamReader(url.openStream()));
+
+                char[] buffer = new char[1024];
+
+                String jsonString = new String();
+
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while ((line = br.readLine()) != null) {
+                    sb.append(line+"\n");
+                }
+                br.close();
+
+                jsonString = sb.toString();
+
+                System.out.println("JSON: " + jsonString);
+
+
+
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             } catch (IOException e) {
